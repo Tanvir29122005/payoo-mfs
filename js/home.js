be1810c81e8437552cfd75c2189479619a533382
mohammad tanvir <mohammadtanvirtaher2005@gmail.com>
2025-03-22 02:51:10 +0600

    create the file

---
+++ js/home.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addMoney = document.getElementById('input-add-money').Value;
+    const pinNumber = document.getElementById('input-pin-number').Value;
+});
